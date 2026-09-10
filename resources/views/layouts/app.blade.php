b4241657cfa58c739d8f036024c824e6faf8e535
Muhammad Syafiq Akmal <m.syafiq.akmal2@gmail.com>
2026-09-10 11:44:27 +0700

    apaaja

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -578,18 +578,22 @@
          PRELOADER
     ================================= --}}
 
-    @if (request()->is('mahasiswa'))
     <div class="preloader">
-        <div class="loader-counter">
-            <span id="loaderCounter">0</span>
-            <span>%</span>
+
+        <div class="loader-number">
+            <span id="loaderCounter">0</span>%
         </div>
 
-        <div class="loader-bar">
-            <div id="loaderProgress"></div>
+        <div class="loader-text">
+            LOADING STUDENT SYSTEM
         </div>
+
+        <div class="loader-line"></div>
+
+        <div class="loader-progress" id="loaderProgress"></div>
+
     </div>
-@endif
+
 
     {{-- =================================
          NAVBAR
